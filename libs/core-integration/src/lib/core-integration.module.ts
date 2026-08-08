7a2a7b0bdb6a7c3a006885b4b1738c22e75f8811
David de la Cruz Gracia <david.gracia@biocatch.com>
2026-08-08 21:15:42 +0200

    refactor: relocate sandbox module into core-integration lib

--- libs/core-integration/src/lib/core-integration.module.ts
+++ libs/core-integration/src/lib/core-integration.module.ts
@@ -3,8 +3,15 @@ import { AzureModule } from './azure/azure.module';
 import { ArtifactsModule } from './artifacts/artifacts.module';
 import { AlertsModule } from './alerts/alerts.module';
 import { WebhooksModule } from './webhooks/webhooks.module';
+import { SandboxModule } from './sandbox/sandbox.module';
 
 @Module({
-  imports: [AzureModule, ArtifactsModule, AlertsModule, WebhooksModule],
+  imports: [
+    AzureModule,
+    ArtifactsModule,
+    AlertsModule,
+    WebhooksModule,
+    SandboxModule,
+  ],
 })
 export class CoreIntegrationModule {}
